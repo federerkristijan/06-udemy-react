67cd10f1e3fea767e215c9c7e82422cfca672f7c
Kristijan Federer <federer.kristijan@gmail.com>
2022-07-12 12:44:45 +0200

    errors

--- src/App.js
+++ src/App.js
@@ -1,36 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   // an alternative to .then blocks is
   async function fetchMoviesHandler() {
     setIsLoading(true);
-     const response = await fetch('https://swapi.dev/api/films/')
-     const data = await response.json();
+    setError(null);
 
-  // function fetchMoviesHandler() {
-  //   fetch('https://swapi.dev/api/films/')
-  //     .then((response) => {
-  //       // transforms json data into JS object
-  //       return response.json();
-  //     })
+    // if .then, then use .catch()
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error('Somethign went wrong!');
+      }
+
+      const data = await response.json();
+      // function fetchMoviesHandler() {
+      //   fetch('https://swapi.dev/api/films/')
+      //     .then((response) => {
+      //       // transforms json data into JS object
+      //       return response.json();
+      //     })
       // .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-        setIsLoading(false);
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
       // });
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }
+
+  let content = <p>Found no movies</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (movies.length === 0) {
+    content = <p>No movies to show.</p>
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -39,10 +68,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {/* if the data is not loading, i.e .finished loading, show movies list */}
-        {!isLoading && <MoviesList movies={movies} /> }
-        {!isLoading && movies.length === 0 && <p>No movies to show.</p>}
-        {isLoading && <p>Loading...</p>}
+        {content}
       </section>
     </React.Fragment>
   );
